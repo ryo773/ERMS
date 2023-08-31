f8ad7b62470a64c98f5a194a2582a7d6abe8b7fa
ryo773 <ryohei2323dat@gmail.com>
2023-08-31 18:05:10 +0900

    seederエラー修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,11 +10,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        Event::factory(100)->create();
-        
         $this->call([
             UserSeeder::class,
-            ReservationSeeder::class,
             ]);
         
     }
